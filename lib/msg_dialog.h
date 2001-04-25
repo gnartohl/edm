495eee39cbff5afdd236b89c86a49fd824d41ba7
sinclair <>
2001-04-25 21:54:21 +0000

    for new color stuff

---
+++ lib/msg_dialog.h
@@ -0,0 +1,70 @@
+//  edm - extensible display manager
+
+//  Copyright (C) 1999 John W. Sinclair
+
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+#ifndef __msg_dialog_h
+#define __msg_dialog_h 1
+
+#include <Xm/Xm.h>
+#include <Xm/MainW.h>
+#include <Xm/BulletinB.h>
+#include <Xm/DrawingA.h>
+#include <Xm/PushBG.h>
+#include <Xm/ArrowBG.h>
+#include <Xm/Label.h>
+#include <Xm/Frame.h>
+#include <Xm/RowColumn.h>
+#include <Xm/DialogS.h>
+#include <Xm/PanedW.h>
+#include <Xm/TextF.h>
+#include <Xm/Text.h>
+#include <Xm/Form.h>
+
+#include "font_pkg.h"
+#include "msg_dialog.str"
+
+class msgDialogClass {
+
+private:
+
+Display *display;
+Widget shell, labelForm, label;
+int x, y, winOpen;
+
+public:
+
+msgDialogClass::msgDialogClass ( void );
+
+msgDialogClass::~msgDialogClass ( void );
+
+int msgDialogClass::destroy ( void );
+
+int msgDialogClass::create (
+  Widget top );
+
+int msgDialogClass::popup (
+  char *text,
+  int _x,
+  int _y );
+
+int msgDialogClass::popdown ( void );
+
+Widget msgDialogClass::top ( void );
+
+};
+
+#endif
