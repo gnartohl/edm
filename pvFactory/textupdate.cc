e6383aa37ccd0ab36aaa6b3bc0a3c635849290aa
kasemir <>
2002-07-12 17:36:34 +0000

    Changed the highlight border: Default was 2. 5 Is a litte too fat, eating into the real estate of the widget. Now it's 3

--- pvFactory/textupdate.cc
+++ pvFactory/textupdate.cc
@@ -992,7 +992,7 @@ int edmTextentryClass::activate(int pass, void *ptr)
                                              XmNuserData,
                                                  this,// obj accessible to d&d
                                              XmNhighlightThickness,
-                                                 (XtArgVal) 5,
+                                                 (XtArgVal) 3,
                                              NULL);
             // callback: text entered ==> send it to the PV
             XtAddCallback(widget, XmNactivateCallback,
