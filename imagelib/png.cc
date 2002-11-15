3e87ec919b6de6f38a46abd3f783b6d5c410912e
sinclair <>
2002-11-15 17:20:40 +0000

    Added code to enforce forward incompatability when a major version number in an edl file is greater than the current version of the executable.

--- imagelib/png.cc
+++ imagelib/png.cc
@@ -1145,6 +1145,11 @@ int major, minor, release;
 
   fscanf( f, "%d %d %d\n", &major, &minor, &release ); actWin->incLine();
 
+  if ( major > APNGC_MAJOR_VERSION ) {
+    postIncompatable();
+    return 0;
+  }
+
   fscanf( f, "%d\n", &x ); actWin->incLine();
   fscanf( f, "%d\n", &y ); actWin->incLine();
   fscanf( f, "%d\n", &w ); actWin->incLine();
