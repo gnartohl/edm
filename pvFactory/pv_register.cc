7510bfa02bd27f3448e5252f80874518182268f4
sinclair <>
2006-12-06 17:50:45 +0000

    pv_register.cc: add support for function name reporting, add author
    pv_register.str: added

--- pvFactory/pv_register.cc
+++ pvFactory/pv_register.cc
@@ -3,52 +3,61 @@
 #include"regTextupdate.h"
 #include"strip.h"
 #include"byte.h"
+#include"environment.str"
+#include"pv_register.str"
 
 // --------------------------------------------------------
 // Registration
 // --------------------------------------------------------
 
-//typedef struct
-//{
-//    char *className;
-//    char *typeName;
-//    char *text;
-//} libRecType;
+typedef struct
+{
+    char *className;
+    char *typeName;
+    char *text;
+} libRecType;
 
-//static int libRecIndex = 0;
+static int libRecIndex = 0;
 
-//static libRecType exported[] =
-//{
-//    { TEXTUPDATE_CLASSNAME, "Monitors", "Textupdate" },
-//    { REGTEXTUPDATE_CLASSNAME, "Monitors", "RegTextupdate" },
-//    { TEXTENTRY_CLASSNAME, "Controls", "Textentry" },
-//    { STRIP_CLASSNAME, "Monitors", "Stripchart" },
-//    { BYTE_CLASSNAME, "Monitors", "Byte" }
-//};
+static libRecType exported[] =
+{
+    { TEXTUPDATE_CLASSNAME, global_str2, reg_str1 },
+    { REGTEXTUPDATE_CLASSNAME, global_str2, reg_str2 },
+    { TEXTENTRY_CLASSNAME, global_str5, reg_str3 },
+    { BYTE_CLASSNAME, global_str2, reg_str4 }
+};
 
 extern "C"
 {
 
-  //    int firstRegRecord(char **className, char **typeName, char **text)
-  //    {
-  //        *className = exported[0].className;
-  //        *typeName  = exported[0].typeName;
-  //        *text      = exported[0].text;
-  //        libRecIndex = 1;
-  //        return 0;
-  //    }
-  //
-  //    int nextRegRecord(char **className, char **typeName, char **text)
-  //    {
-  //        int max = sizeof(exported) / sizeof(exported[0]);
-  //        if (libRecIndex >= max)
-  //            return -1; //no more 
-  //        *className = exported[libRecIndex].className;
-  //        *typeName  = exported[libRecIndex].typeName;
-  //        *text      = exported[libRecIndex].text;
-  //        libRecIndex++;
-  //        return 0;
-  //    }
+    char *author ( void ) {
+
+    static char *a = "Kay Kasemir (kasemirk@ornl.gov)";
+
+      return a;
+
+    }
+
+    int firstRegRecord(char **className, char **typeName, char **text)
+    {
+        *className = exported[0].className;
+        *typeName  = exported[0].typeName;
+        *text      = exported[0].text;
+        libRecIndex = 1;
+        return 0;
+    }
+  
+    int nextRegRecord(char **className, char **typeName, char **text)
+    {
+        int max = sizeof(exported) / sizeof(exported[0]);
+        if (libRecIndex >= max)
+            return -1; //no more 
+        *className = exported[libRecIndex].className;
+        *typeName  = exported[libRecIndex].typeName;
+        *text      = exported[libRecIndex].text;
+        libRecIndex++;
+        return 0;
+    }
 
     void *create_TextupdateClassPtr (void)
     {
@@ -88,19 +97,6 @@ extern "C"
         edmTextentryClass *obj = new edmTextentryClass(src);
         return (void *) obj;
     }
-
-  //    void *create_StripClassPtr (void)
-  //    {
-  //        edmStripClass *obj = new edmStripClass;
-  //        return (void *) obj;
-  //    }
-  //    
-  //    void *clone_StripClassPtr (void *rhs)
-  //    {
-  //        edmStripClass *src = (edmStripClass *) rhs;
-  //        edmStripClass *obj = new edmStripClass(src);
-  //        return (void *) obj;
-  //    }
 
     void *create_ByteClassPtr (void)
     {
