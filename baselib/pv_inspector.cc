196d2ed871b049f343920da0b4b6113ab7d3dfc9
sinclair <>
2008-12-12 16:56:47 +0000

    Version 1-11-1y
    pv_inspector.cc, related_display.cc, act_win.cc: fixed issue where related displays were not reliably closing when opened with the "on focus" or "right button popup" options. Thanks to Brian Bevins from JLab.

--- baselib/pv_inspector.cc
+++ baselib/pv_inspector.cc
@@ -615,8 +615,13 @@ activeWindowListPtr cur;
     }
 
     if ( okToClose ) {
-      aw->returnToEdit( 1 );
-      aw = NULL;
+      if ( aw->okToDeactivate() ) {
+        aw->returnToEdit( 1 );
+        aw = NULL;
+      }
+      else {
+        aw->closeDeferred( 20 );
+      }
     }
 
   }
@@ -2656,8 +2661,16 @@ activeWindowListPtr cur;
       }
 
       if ( okToClose ) {
-        aw->returnToEdit( 1 );
-        aw = NULL;
+        if ( aw->okToDeactivate() ) {
+          aw->returnToEdit( 1 );
+          aw = NULL;
+	}
+        else {
+          actWin->appCtx->proc->lock();
+          needClose = 1;
+          actWin->addDefExeNode( aglPtr );
+          actWin->appCtx->proc->unlock();
+	}
       }
       else {
         aw = NULL;
